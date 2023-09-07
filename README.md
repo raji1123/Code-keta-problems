347db6ab54b7da7715c2f52f37be54cfdd5e34b4
raji1123 <rajilakshmi1112223@gmail.com>
2023-09-08 00:09:06 +0530

    ss

--- README.md
+++
@@ -1,1 +0,0 @@
-# Code-keta-problems
---
+++ src/test/Sample.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 4;
+		
+		for (int i=1; i<= 10; i++) {
+			if (i != a) {
+				System.out.println(i);
+			}
+			else {
+				System.out.println("Value is 4");
+			}
+		}
+		
+	}
+
+}
